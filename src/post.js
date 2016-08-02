6bf6aa1872d2f4ae19c93b1adf59777daa312d77
Osshrp <osshrp@gmail.com>
2016-08-02 14:32:11 +0300

    Add validation

--- src/post.js
+++ src/post.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-class Post extends React.Component {
-  render() {
-    var newsNodes = this.props.news.map(function(item) {
+// class Post extends React.Component {
+var Post = React.createClass({
+  propTypes: {
+    news: React.PropTypes.arrayOf(React.PropTypes.shape({
+      id: React.PropTypes.number.isRequired,
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string
+    }))
+  },
+  render: function() {
+    var newsNodes = this.props.news.map(function(item, key) {
       return (
-        <div>
+        <div key = {key}>
           <p>{item.name}:</p>
           <p>{item.description}</p>
         </div>
@@ -16,6 +24,6 @@ class Post extends React.Component {
       </div>
     )
   }
-}
+})
 
 export default Post
